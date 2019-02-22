acd1519f188fd366ced588443f0d8f2c2db4f75b
VicziTomi <viczian.t@gmail.com>
2019-02-22 10:48:44 +0100

    modifications

--- car_movement2.js
+++ car_movement2.js
@@ -22,10 +22,10 @@ function displayWestCar (array) {
 }
 print2D.print2D(displayWestCar(finalarray.finalarray));
 
-function moveEast (array, x, y, EastCar) {
-  array[x][y] -= EastCar;
-  array[x][y + 1] = EastCar;
+function moveEast (array, y, x, EastCar) {
+  array[y][x] -= EastCar;
+  array[y][x + 1] = EastCar;
   CarMovements[0] = [x, y + 1, EastCar];
   return array;
 }
-moveEast(displayWestCar());
+moveEast(displayWestCar(finalarray.finalarray), CarMovements[0][0], CarMovements[0][1], CarMovements[0][2]);
