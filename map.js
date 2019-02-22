b570c040fdc96590efa4e6733b3402f8fb5023d7
VicziTomi <viczian.t@gmail.com>
2019-02-22 15:39:05 +0100

    map ready

--- map.js
+++ map.js
@@ -7,13 +7,21 @@ function genArr (size) {
 }
 let lamp = 'P';
 let road = 0;
-let empty = '';
+let empty = ' ';
 function buildMap (array) {
+  for (let y = 0; y < array.length; y++) {
+    for (let x = 0; x < array[y].length; x++) {
+      array[14][x] = road;
+      array[15][x] = road;
+      array[y][14] = road;
+      array[y][15] = road;
+      array[y][x] = empty;
+    }
+  }
   array[13][13] = lamp;
   array[13][16] = lamp;
   array[16][13] = lamp;
   array[16][16] = lamp;
-
   return array;
 }
 let map = buildMap(genArr(30));
