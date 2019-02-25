3d203d3182d99d02d44284d23bd969bd5f0c081d
Juhi22 <j.dani22@freemail.hu>
2019-02-25 21:22:04 +0100

    moveThemAll

--- main.js
+++ main.js
@@ -34,10 +34,15 @@ process.stdin.on('keypress', function (ch, key) {
   }
 });
 
+function efd (térkép) {
+  console.clear();
+  console.log(térkép);
+}
+
 function asd (cars) {
   console.log(cars);
 }
-
+setInterval(function () { efd(map.map); }, 100);
 setInterval(function () { asd(randomCar.myCars); }, 3000);
 
 buttonpress();
--- moveFunction.js
+++ moveFunction.js
@@ -4,17 +4,17 @@ const map = require('./map');
 const move = (array) => { // array = a különböző autók
   for (let i = 0; i < array.length; i++) {
     if ((array[i][0] <= 14 || array[i][0] >= 16) && array[i][1] === 14) {
-      // moveSouth(array[i], map.map); // délre mozgás crossroads előtt/után
+      moveSouth(array[i], map.map); // délre mozgás crossroads előtt/után
     } else if ((array[i][0] >= 15 || array[i][0] <= 13) && array[i][1] === 15) {
-      // moveNorth(array[i], map.map); // északra mozgás crossroads előtt/után
+      moveNorth(array[i], map.map); // északra mozgás crossroads előtt/után
     } else if ((array[i][1] >= 15 || array[i][1] <= 13) && array[i][0] === 14) {
-      // moveWest(array[i], map.map); // nyugatra mozgás crossroads előtt/után
+      moveWest(array[i], map.map); // nyugatra mozgás crossroads előtt/után
     } else if ((array[i][1] <= 14 || array[i][1] >= 16) && array[i][0] === 15) {
-      return moveEast(array[i], map.map); // keletre mozgás crossroads előtt/után
+      moveEast(array[i], map.map); // keletre mozgás crossroads előtt/után
     } else if (array[i][0] === 14 && array[i][1] === 14 && array[i][2] === 4) {
-      // moveWest(array[i], map.map); // bal felsőből nyugatra
+      moveWest(array[i], map.map); // bal felsőből nyugatra
     } else if (array[i][0] === 14 && array[i][1] === 14 && array[i][2] !== 4) {
-      // moveSouth(array[i], map.map); // bal felsőből délre
+      moveSouth(array[i], map.map); // bal felsőből délre
     }
   }
 };
@@ -23,7 +23,31 @@ function moveEast (car, map) { // visz keletre függvény
   let temp = map[car[0]][car[1]];
   map[car[0]][car[1]] = map[car[0]][car[1] + 1];
   map[car[0]][car[1] + 1] = temp; // megtörtént a csere
-  car[1] += 1;
+  car[1] += 1; // értéket növel
+  return map; // friss mapot adja vissza
+}
+
+function moveSouth (car, map) {
+  let temp = map[car[0]][car[1]];
+  map[car[0]][car[1]] = map[car[0] + 1][car[1]];
+  map[car[0] + 1][car[1]] = temp;
+  car[0] += 1;
+  return map;
+}
+
+function moveNorth (car, map) {
+  let temp = map[car[0]][car[1]];
+  map[car[0]][car[1]] = map[car[0] - 1][car[1]];
+  map[car[0] - 1][car[1]] = temp;
+  car[0] -= 1;
+  return map;
+}
+
+function moveWest (car, map) {
+  let temp = map[car[0]][car[1]];
+  map[car[0]][car[1]] = map[car[0]][car[1] - 1];
+  map[car[0]][car[1] - 1] = temp;
+  car[1] -= 1;
   return map;
 }
 
