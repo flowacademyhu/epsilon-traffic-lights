dda07a216dea5a5dcb3bdc3ba2127be7f59a90e1
VicziTomi <viczian.t@gmail.com>
2019-02-27 14:52:11 +0100

    prettymap in progress

--- frontMap.js
+++ frontMap.js
@@ -1,20 +1,31 @@
-const map = require('./map');
+let map = require('./map');
 
-const frontMap = (backMap) => {
-  let resultString = '';
-
+function frontMap (backMap) {
+  let prettyPrint;
   for (let i = 0; i < backMap.length; i++) {
-    let row = [];
     for (let j = 0; j < backMap[i].length; j++) {
-      row.push(backMap[i][j]);
+      if (backMap[i][j] === ' ') {
+        prettyPrint += '░';
+      } else if (backMap[i][j] === 0) {
+        prettyPrint += '▓';
+      } else if (backMap[i][j] === 1) {
+        prettyPrint += '¥';
+      } else if (backMap[i][j] === 2) {
+        prettyPrint += '©';
+      } else if (backMap[i][j] === 3) {
+        prettyPrint += 'Þ';
+      } else if (backMap[i][j] === 4) {
+        prettyPrint += '×';
+      } else if (backMap[i][j] === 'Z') {
+        prettyPrint += 'Z';
+      } else if (backMap[i][j] === 'P') {
+        prettyPrint += 'P';
+      }
     }
-    resultString += row + '\n';
+    prettyPrint += '\n';
   }
-  return resultString;
-};
-
-// setInterval(function () { frontMap(map); }, 10);
-
-// console.log(frontMap(map.map));
+  return prettyPrint;
+}
+console.log(frontMap(map.map));
 
 module.exports = { frontMap };
